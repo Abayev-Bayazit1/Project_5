85768a066b268fdd09053645a839e1ab82d7b7a8
Abayev Bayazit <240918@astanait.edu.kz>
2025-01-19 00:09:19 +0500

    Connected Database

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-
+import data.PostgreDB;
+import data.interfaces.IDB;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+    IDB db = new PostgreDB("jdbc:postgresql://localhost:5432", "postgres", "0000", "Booking");
+
     }
-
-
-
 }
