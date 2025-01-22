11351a477be4693fd8ba1036ea9a61bcb724026a
Abayev <240918@astanait.edu.kz>
2025-01-22 20:47:07 +0500

    HotelRepository

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import data.PostgreDB;
 import data.interfaces.IDB;
+import repository.HotelRepository;
+import repository.interfaces.IHotelRepository;
 
 public class Main {
     public static void main(String[] args) {
     IDB db = new PostgreDB("jdbc:postgresql://localhost:5432", "postgres", "0000", "Booking");
+   // IHotelRepository repo = new HotelRepository(db);
+
+
+
+
 
     }
 }
--- src/repository/HotelRepository.java
+++ src/repository/HotelRepository.java
@@ -1,5 +1,66 @@
 package repository;
 
-public class HotelRepository {
+import data.interfaces.IDB;
+import models.Hotel;
+import repository.interfaces.IHotelRepository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelRepository implements IHotelRepository {
+
+    private final IDB db;
+
+
+    public HotelRepository(IDB db) {
+        this.db = db;
+    }
+
+
+    @Override
+    public boolean addHotel(Hotel hotel) {
+        Connection conn = null;
+
+        try{
+            conn = db.getConnection();
+            String sql  = "INSERT INTO hotels (name, address ) VALUES (?,?)";
+            PreparedStatement st  = conn.prepareStatement(sql);
+            st.setString(1,hotel.getName());
+            st.setString(2, hotel.getAddress());
+            return st.executeUpdate() > 0;
+
+        }catch (Exception e){
+            System.out.println("sql error " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public List<Hotel> getAllHotels() {
+        Connection conn = null;
+
+        try{
+            conn = db.getConnection();
+            String sql = "SELECT * FROM hotels";
+            Statement st = conn.createStatement();
+
+            ResultSet rs = st.executeQuery(sql);
+            List<Hotel> hotels = new ArrayList<>();
+            while (rs.next()) {
+                Hotel hotel = new Hotel(rs.getInt("id"),rs.getString("name"), rs.getString("address"));
+
+                hotels.add(hotel);
+            }
+
+            return hotels;
+        }catch(Exception e){
+            System.out.println("sql error " + e.getMessage());
+            }
+
+    return null;
+    }
 }
